0c50ecce66d83eb020f1e515701476ff8bdbc8b1
Brett Nash <nash@nash.space>
2021-07-05 14:33:02 -0700

    trtl_object: Format.

--- trtl_object.h
+++ trtl_object.h
@@ -10,8 +10,7 @@ struct swap_chain_data;
 struct trtl_object {
 	// Interface
 	void (*draw)(struct trtl_object *obj, VkCommandBuffer cmd_buffer,
-		VkPipelineLayout  pipeline_layout,
-		int32_t offset);
+		     VkPipelineLayout pipeline_layout, int32_t offset);
 	uint32_t (*vertices)(struct trtl_object *obj, struct vertex **vertices);
 	uint32_t (*indices)(struct trtl_object *obj, uint32_t **indices);
 	bool (*update)(struct trtl_object *obj, int frame);
@@ -27,13 +26,13 @@ struct trtl_object {
 
 	VkDeviceMemory texture_image_memory;
 	VkImage texture_image;
-	VkImageView texture_image_view;	
+	VkImageView texture_image_view;
 
 	struct trtl_uniform_info *uniform_info;
-	
+
 	bool reverse;
 };
 
 // Once again: concreate implementation
-struct trtl_object *
-trtl_object_create(void *ctx, struct swap_chain_data *scd, const char *path, const char *texture);
+struct trtl_object *trtl_object_create(void *ctx, struct swap_chain_data *scd, const char *path,
+				       const char *texture);
