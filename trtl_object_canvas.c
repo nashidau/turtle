f53d3f050d9b9081c10d7cd29f0b0a94dd9045b7
Brett Nash <nash@nash.space>
2021-08-21 21:34:12 -0700

    canvas: Tweak size of canvas

--- trtl_object_canvas.c
+++ trtl_object_canvas.c
@@ -31,10 +31,10 @@ trtl_alloc static VkDescriptorSet *create_descriptor_sets(struct trtl_object_can
 // allow 2d objects to be return from indices get.
 // static const struct vertex2d vertices[] = {
 static const struct vertex canvas_vertices[] = {
-    {{-0.5f, -0.5f, 0}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+    {{-1.0f, -0.5f, 0}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
     {{0.5f, -0.5f, 0}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
     {{0.5f, 0.5f, 0}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
-    {{-0.5f, 0.5f, 0}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}},
+    {{-1.0f, 0.5f, 0}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}},
 };
 
 static const uint32_t canvas_indices[] = {0, 1, 2, 2, 3, 0};
