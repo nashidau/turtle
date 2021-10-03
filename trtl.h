55e32385233bcd0e198dcc107765b0dc2dcce729
Brett Nash <nash@nash.space>
2021-10-02 19:56:43 -0700

    helpers: Comment out aanti-malloc headers

--- trtl.h
+++ trtl.h
@@ -1,7 +1,7 @@
 
 
-#define calloc(x, y) ILLEGAL_TALLOC_CALL
-#define free(x) ILLEGAL_FREE_CALL
-#define malloc(x) ILLEGAL_MALLOC_CALL
-#define realloc(x, y) ILLEGAL_REALLOC_CALL
+//#define calloc(x, y) ILLEGAL_TALLOC_CALL
+//#define free(x) ILLEGAL_FREE_CALL
+//#define malloc(x) ILLEGAL_MALLOC_CALL
+//#define realloc(x, y) ILLEGAL_REALLOC_CALL
 
