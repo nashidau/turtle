ef7fb74d3db0908b4a73204f3e23020eaebba912
Brett Nash <nash@nash.space>
2021-10-14 21:53:32 -0700

    trtl_check: Enable leak report in trtl_check.

--- trtl_check.c
+++ trtl_check.c
@@ -18,22 +18,20 @@ static test_module test_modules[] = {
 #define N_MODULES ((int)(sizeof(test_modules)/sizeof(test_modules[0])))
 
 int main(trtl_arg_unused int argc, trtl_arg_unused char **argv) {
-	void *ctx;
 	SRunner *sr;
 	int nfailed;
 	int i;
 
-	ctx = talloc_init("Test Context");
+	talloc_enable_leak_report();
 
 	sr = srunner_create(NULL);
 	for (i = 0 ; i < N_MODULES ; i ++) {
-		srunner_add_suite(sr, test_modules[i](ctx));
+		srunner_add_suite(sr, test_modules[i](NULL));
 	}
 
 	srunner_run_all(sr, CK_VERBOSE);
 	nfailed = srunner_ntests_failed(sr);
 	srunner_free(sr);
-	talloc_free(ctx);
 
 	return !!nfailed;
 }
