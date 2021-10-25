9bde121f086b8a0370052b6ee6bf4c8837ceab1a
Brett Nash <nash@nash.space>
2021-10-24 21:15:15 -0700

    sprite: Actually add the image/texture.

--- trtl_object_sprite.c
+++ trtl_object_sprite.c
@@ -6,13 +6,17 @@
 #include "trtl_object.h"
 #include "trtl_object_sprite.h"
 #include "trtl_pipeline.h"
-#include "trtl_shell.h"
 #include "trtl_seer.h"
+#include "trtl_shell.h"
+#include "trtl_texture.h"
 #include "trtl_uniform.h"
 #include "turtle.h"
 #include "vertex.h"
 
 #include <talloc.h>
+
+#define VERTEX_SHADER "shaders/sprite/sprite-vertex.spv"
+#define FRAGMENT_SHADER "shaders/sprite/sprite-fragment.spv"
 
 struct trtl_object_sprite {
 	struct trtl_object parent;
@@ -26,6 +30,11 @@ struct trtl_object_sprite {
 	struct trtl_pipeline_info pipeline_info;
 	uint32_t vcount;
 	uint32_t icount;
+
+	// FIXME: This so needs to be abstraced away
+	VkDeviceMemory texture_image_memory;
+	VkImage texture_image;
+	VkImageView texture_image_view;
 
 	VkBuffer index_buffer;
 	VkBuffer vertex_buffer;
@@ -48,8 +57,6 @@ struct sprite_vertex {
 	struct pos2d tex_coord;
 };
 
-
-
 static const VkVertexInputAttributeDescription sprite_vertex_description[3] = {
     {
 	.binding = 0,
@@ -60,7 +67,7 @@ static const VkVertexInputAttributeDescription sprite_vertex_description[3] = {
     {
 	.binding = 0,
 	.location = 1,
-	.format = VK_FORMAT_R32G32B32_UINT,
+	.format = VK_FORMAT_R32G32B32_SFLOAT,
 	.offset = offsetof(struct sprite_vertex, tile),
     },
     {
@@ -80,13 +87,13 @@ static const VkVertexInputBindingDescription sprite_binding_descriptor = {
 
 #define N_VERTEX_ATTRIBUTE_DESCRIPTORS TRTL_ARRAY_SIZE(sprite_vertex_description)
 
-
-
 static const uint32_t sprite_indices[] = {0, 1, 2, 3, 2, 0};
 static const uint32_t CANVAS_OBJECT_NINDEXES = TRTL_ARRAY_SIZE(sprite_indices);
 
 trtl_alloc static VkDescriptorSet *sprite_create_descriptor_sets(struct trtl_object_sprite *sprite,
 								 struct trtl_swap_chain *scd);
+
+static VkDescriptorSetLayout sprite_create_descriptor_set_layout(VkDevice device);
 
 // Inline function to cast from abstract to concrete type.
 // FIXME: Make Debug and non-debug do different things
@@ -131,16 +138,20 @@ sprite_relayer(struct trtl_object *obj, struct turtle *turtle,
 {
 
 	struct trtl_object_sprite *sprite = trtl_object_sprite(obj);
-	// sprite->descriptor_set_layout = sprite_create_descriptor_set_layout(turtle->device);
+	sprite->descriptor_set_layout = sprite_create_descriptor_set_layout(turtle->device);
 	sprite->descriptor_set = sprite_create_descriptor_sets(sprite, turtle->tsc);
+
+	printf("sprite pipeline info %p %p %d/%d %p %s %s %p %p %d\n",
+	    turtle, renderpass, size.width, size.height, sprite->descriptor_set_layout,
+	    VERTEX_SHADER, FRAGMENT_SHADER, &sprite_binding_descriptor, &sprite_vertex_description,
+	    N_VERTEX_ATTRIBUTE_DESCRIPTORS);
 	sprite->pipeline_info = trtl_pipeline_create(
-	    turtle, renderpass, size, sprite->descriptor_set_layout,
-	    "shaders/sprite/sprite-vertex.spv", "shaders/sprite/sprite-fragment.spv",
+	    turtle, renderpass, size, sprite->descriptor_set_layout, VERTEX_SHADER, FRAGMENT_SHADER,
 	    &sprite_binding_descriptor, sprite_vertex_description, N_VERTEX_ATTRIBUTE_DESCRIPTORS);
 }
 
 struct trtl_object *
-trtl_object_sprite_create(struct turtle *turtle, trtl_arg_unused const char *image)
+trtl_object_sprite_add(struct turtle *turtle, trtl_arg_unused const char *image)
 {
 	struct trtl_object_sprite *sprite;
 
@@ -149,6 +160,18 @@ trtl_object_sprite_create(struct turtle *turtle, trtl_arg_unused const char *ima
 	sprite->parent.draw = sprite_draw;
 	sprite->parent.update = sprite_update;
 	sprite->parent.relayer = sprite_relayer;
+
+	sprite->nframes = turtle->tsc->nimages;
+
+	// FIXME: Leaky on create_texture_image
+	sprite->texture_image_view =
+	    create_texture_image_view(turtle, create_texture_image(turtle, image));
+
+	sprite->uniform_info =
+	    trtl_uniform_alloc_type(turtle->uniforms, struct UniformBufferObject);
+
+	sprite->descriptor_set_layout = sprite_create_descriptor_set_layout(turtle->device);
+	sprite->descriptor_set = sprite_create_descriptor_sets(sprite, turtle->tsc);
 
 	{
 		struct trtl_seer_vertexset vertices;
@@ -167,6 +190,38 @@ trtl_object_sprite_create(struct turtle *turtle, trtl_arg_unused const char *ima
 	}
 
 	return &sprite->parent;
+}
+
+static VkDescriptorSetLayout
+sprite_create_descriptor_set_layout(VkDevice device)
+{
+	VkDescriptorSetLayout descriptor_set_layout;
+
+	VkDescriptorSetLayoutBinding ubo_layout_binding = {0};
+	ubo_layout_binding.binding = 0;
+	ubo_layout_binding.descriptorCount = 1;
+	ubo_layout_binding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+	ubo_layout_binding.pImmutableSamplers = NULL;
+	ubo_layout_binding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+
+	VkDescriptorSetLayoutBinding sampler_layout_binding = {0};
+	sampler_layout_binding.binding = 1;
+	sampler_layout_binding.descriptorCount = 1;
+	sampler_layout_binding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+	sampler_layout_binding.pImmutableSamplers = NULL;
+	sampler_layout_binding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+
+	VkDescriptorSetLayoutBinding bindings[2] = {ubo_layout_binding, sampler_layout_binding};
+	VkDescriptorSetLayoutCreateInfo layoutInfo = {0};
+	layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+	layoutInfo.bindingCount = TRTL_ARRAY_SIZE(bindings);
+	layoutInfo.pBindings = bindings;
+
+	if (vkCreateDescriptorSetLayout(device, &layoutInfo, NULL, &descriptor_set_layout) !=
+	    VK_SUCCESS) {
+		error("failed to create descriptor set layout!");
+	}
+	return descriptor_set_layout;
 }
 
 trtl_alloc static VkDescriptorSet *
@@ -194,7 +249,12 @@ sprite_create_descriptor_sets(struct trtl_object_sprite *sprite, struct trtl_swa
 		VkDescriptorBufferInfo buffer_info =
 		    trtl_uniform_buffer_get_descriptor(sprite->uniform_info, i);
 
-		VkWriteDescriptorSet descriptorWrites[1] = {0};
+		VkDescriptorImageInfo image_info = {0};
+		image_info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+		image_info.imageView = sprite->texture_image_view;
+		image_info.sampler = scd->turtle->texture_sampler;
+
+		VkWriteDescriptorSet descriptorWrites[2] = {0};
 
 		descriptorWrites[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
 		descriptorWrites[0].dstSet = sets[i];
@@ -203,6 +263,14 @@ sprite_create_descriptor_sets(struct trtl_object_sprite *sprite, struct trtl_swa
 		descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 		descriptorWrites[0].descriptorCount = 1;
 		descriptorWrites[0].pBufferInfo = &buffer_info;
+
+		descriptorWrites[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		descriptorWrites[1].dstSet = sets[i];
+		descriptorWrites[1].dstBinding = 1;
+		descriptorWrites[1].dstArrayElement = 0;
+		descriptorWrites[1].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+		descriptorWrites[1].descriptorCount = 1;
+		descriptorWrites[1].pImageInfo = &image_info;
 
 		vkUpdateDescriptorSets(scd->turtle->device, TRTL_ARRAY_SIZE(descriptorWrites),
 				       descriptorWrites, 0, NULL);
