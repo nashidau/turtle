e7fdef77046b398156014f23dabb74d4314ada06
Brett Nash <nash@nash.space>
2021-11-09 18:38:12 -0800

    fake-shadercc: Add a fake shader CC for unit testing
    Github LFS is totally useless, and woefully overpriced.  Testing doesn't
    use the real shaders anyway, so just use a fake compiler.

---
+++ tools/fake-shadercc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Github LFS has stupidly low limits.  Fortunately for unit tests we don't need a shader compiler,
+# just something that generates a file like glslc.
+#
+#  fake-shadercc.sh -o ${OUT} ${IN}
+
+while getopts o: flag
+do
+    case "${flag}" in
+        o) outfilename=${OPTARG};;
+    esac
+done
+shift $((OPTIND-1))
+infilename=$1
+
+echo Fake shader Compiler  $infilename to $oufilename
+cat $infilename > $outfilename
+
+
+
