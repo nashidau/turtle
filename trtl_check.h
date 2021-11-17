b8be5fd812c4e5e72abaf2f58f91ede0479293d9
Brett Nash <nash@nash.space>
2021-11-16 21:25:42 -0800

    check: Hoist common double_eq assert into the header.

--- trtl_check.h
+++ trtl_check.h
@@ -7,3 +7,17 @@
 
 Suite *trtl_uniform_suite(void *ctx);
 Suite *trtl_timer_suite(trtl_arg_unused void *ctx);
+Suite *trtl_shader_suite(trtl_arg_unused void *ctx);
+
+// The ubuntu 20.04 version of check is ancient
+#ifndef ck_assert_double_eq
+#define ck_assert_double_eq(X, Y)                                                                  \
+	do {                                                                                       \
+		double _ck_x = (X);                                                                \
+		double _ck_y = (Y);                                                                \
+		ck_assert_msg(_ck_x == _ck_y, "Assertion '%s' failed: %s == %.*g, %s == %.*g",     \
+			      #X " ==  " #Y, #X, 6, _ck_x, #Y, 6, _ck_y);                          \
+	} while (0)
+#endif
+
+
