d845be1765819d768e9c995598ff30178ed9e7dd
Brett Nash <nash@nash.space>
2021-11-20 15:22:11 -0800

    solo: Documentation addition.

--- trtl_solo.h
+++ trtl_solo.h
@@ -1,3 +1,19 @@
+/**
+ * A number of vilkan commands need to run in a temporary command buffer.
+ *
+ * A solo instance allocates ta a command buffer and starts it. Once complete, by freeing the solo
+ * context causes it to be submitted.
+ *
+ * Useage is thus:
+ * @code
+ * struct trtl_solo *solo = trtl_solo_start();
+ *   // Copy image buffer or similar
+ * talloc_free(solo); // Queue the command buffer and free it.
+ * @endcode
+ *
+ * Note that the talloc_free blocks until the queue has been completed.
+ */
+
 #include <talloc.h>
 #include <vulkan/vulkan.h>
 
