c072661c33698f9a05b2b8e0c6c546a59f7d1d31
Brett Nash <nash@nash.space>
2021-12-29 14:40:49 -0800

    trtl_uniform_check: Pass the first 2 tests with fakes.

--- trtl_uniform_check.c
+++ trtl_uniform_check.c
@@ -4,13 +4,19 @@
 #include <check.h>
 #include <talloc.h>
 
-#include "turtle.h"
-#include "trtl_uniform.h"
 #include "trtl_check.h"
+#include "trtl_uniform.h"
+#include "turtle.h"
 
 struct turtle;
 
-START_TEST(test_uniform_init) {
+FAKE_VOID_FUNC(create_buffer, struct turtle *, VkDeviceSize, VkBufferUsageFlags,
+	       VkMemoryPropertyFlags, VkBuffer *, VkDeviceMemory *);
+FAKE_VOID_FUNC(vkDestroyBuffer, VkDevice, VkBuffer, const VkAllocationCallbacks *);
+FAKE_VOID_FUNC(vkFreeMemory, VkDevice, VkDeviceMemory, const VkAllocationCallbacks*);
+
+START_TEST(test_uniform_init)
+{
 	struct turtle *turtle;
 	struct trtl_uniform *uniforms;
 
@@ -19,9 +25,11 @@ START_TEST(test_uniform_init) {
 	ck_assert_ptr_ne(uniforms, NULL);
 	talloc_free(uniforms);
 	talloc_free(turtle);
-} END_TEST
+}
+END_TEST
 
-START_TEST(test_uniform_alloc) {
+START_TEST(test_uniform_alloc)
+{
 	struct turtle *turtle = talloc_zero(NULL, struct turtle);
 	struct trtl_uniform *uniforms = trtl_uniform_init(turtle, 2, 100);
 
@@ -30,9 +38,11 @@ START_TEST(test_uniform_alloc) {
 
 	// Free should release the info too
 	talloc_free(uniforms);
-} END_TEST
+}
+END_TEST
 
-START_TEST(test_uniform_alloc_too_much) {
+START_TEST(test_uniform_alloc_too_much)
+{
 	struct trtl_uniform *uniforms = trtl_uniform_init(NULL, 2, 100);
 
 	struct trtl_uniform_info *info = trtl_uniform_alloc(uniforms, 96);
@@ -44,11 +54,12 @@ START_TEST(test_uniform_alloc_too_much) {
 
 	// Free should release the info too
 	talloc_free(uniforms);
-} END_TEST
-
+}
+END_TEST
 
 Suite *
-trtl_uniform_suite(trtl_arg_unused void *ctx) {
+trtl_uniform_suite(trtl_arg_unused void *ctx)
+{
 	Suite *s = suite_create("Uniform");
 
 	{
@@ -61,19 +72,6 @@ trtl_uniform_suite(trtl_arg_unused void *ctx) {
 		tcase_add_test(tc_alloc, test_uniform_alloc);
 		tcase_add_test(tc_alloc, test_uniform_alloc_too_much);
 	}
-	/*
-        tcase_add_test(tc_seq, test_seq_single_value);
-        tcase_add_test(tc_seq, test_seq_all_values);
-        tcase_add_test(tc_seq, test_seq_loops);
-        tcase_add_test(tc_seq, test_seq_repeats_last);
-        tcase_add_test(tc_seq, test_seq_change_updates);
-        tcase_add_test(tc_seq, test_seq_change_resets_pos);
-        tcase_add_test(tc_seq, test_seq_empty_fails);
-        tcase_add_test(tc_seq, test_seq_single_item);
-        tcase_add_test(tc_seq, test_seq_range_sane);
-        tcase_add_test(c_seq, test_seq_range_outside);
 
-        tcase_add_test(tc_seq, test_seq_range_all);
-*/
-        return s;
+	return s;
 }
