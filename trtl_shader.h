2df5fd6a7aef3591c020a2bfe7abead07859e878
Brett Nash <nash@nash.space>
2021-12-31 13:47:33 -0800

    shader: Hopefully fix the platform issue for trtl_shader

--- trtl_shader.h
+++ trtl_shader.h
@@ -30,6 +30,14 @@ struct trtl_shader *trtl_shader_get(struct turtle *turtle, const char *path);
 		"_shader_" #NAME "_end:\n"                                                         \
 		".previous\n")
 
+// On mac the underscore is stripped in the assembler (or added by the compiler).  On
+// Linux it seems to preserved.  So adding a _ for Linux
+#ifdef __APPLE__
+#define SYMBOL_PREFIX "_shader_"
+#else
+#define SYMBOL_PREFIX "shader_"
+#endif
+
 // Embeds an inline shader - doesn't set the _end token, sets a length token instead.
 // This is pretty much only for testing.
 #define EMBED_SHADER_STRING(NAME, STRING)                                                          \
@@ -37,9 +45,9 @@ struct trtl_shader *trtl_shader_get(struct turtle *turtle, const char *path);
 	extern uint8_t shader_##NAME##_end;                                                        \
 	__asm__(".data\n"                                                                          \
 		".align 4\n"                                                                       \
-		".global _shader_" #NAME "_start\n"                                                \
-		".global _shader_" #NAME "_end\n"                                                  \
-		"_shader_" #NAME "_start:\n"                                                       \
+		".global " SYMBOL_PREFIX #NAME "_start\n"                                      	   \
+		".global " SYMBOL_PREFIX #NAME "_end\n"                                            \
+		SYMBOL_PREFIX #NAME "_start:\n"                                                    \
 		".asciz \"" STRING "\"\n"                                                          \
-		"_shader_" #NAME "_end:\n"                                                         \
+		SYMBOL_PREFIX #NAME "_end:\n"                                                      \
 		".previous\n")
