1883789ee48153fe2bb74f451214178e4d7ba375
Brett Nash <nash@nash.space>
2022-01-06 20:00:03 -0800

    check: Sort the suites.

--- trtl_check.c
+++ trtl_check.c
@@ -7,17 +7,15 @@
 // FFF needs to inserted somewhere; lets do it here
 DEFINE_FFF_GLOBALS;
 
-Suite *blam_suite(void *ctx);
-
 typedef Suite *(*test_module)(void *ctx);
 
 static test_module test_modules[] = {
 	trtl_crier_suite,
 	trtl_events_suite,
-	trtl_uniform_suite,
 	trtl_shader_suite,
-	trtl_timer_suite,
 	trtl_solo_suite,
+	trtl_timer_suite,
+	trtl_uniform_suite,
 };
 #define N_MODULES ((int)(sizeof(test_modules)/sizeof(test_modules[0])))
 
