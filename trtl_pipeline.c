af40cc5811ebc57b3d3c226502ea52ff42663454
Brett Nash <nash@nash.space>
2022-01-08 21:41:46 -0800

    pipeline: Start work on create_with_strata

--- trtl_pipeline.c
+++ trtl_pipeline.c
@@ -273,3 +273,203 @@ trtl_pipeline_create(struct turtle *turtle, VkRenderPass render_pass, VkExtent2D
 
 	return info;
 }
+
+struct trtl_pipeline_info *
+trtl_pipeline_create_with_strata(struct turtle *turtle, struct trtl_layer *layer,  VkExtent2D extent,
+		VkDescriptorSetLayout descriptor_set_layout,
+		     const char *vertex_shader,
+		     const char *fragment_shader,
+		     const VkVertexInputBindingDescription *binding_description,
+		     const VkVertexInputAttributeDescription *attribute_description,
+		     uint32_t nattributes)
+{
+	struct trtl_pipeline_info *info = talloc_zero(turtle, struct trtl_pipeline_info);
+	info->device = turtle->device;
+	talloc_set_destructor(info, pipeline_destroy);
+
+	struct trtl_shader *vert = talloc_steal(info, trtl_shader_get(turtle, vertex_shader));
+	struct trtl_shader *frag = talloc_steal(info, trtl_shader_get(turtle, fragment_shader));
+
+	// FIXME: Handle this a little more gracefully.
+	assert(vert != NULL);
+	assert(frag != NULL);
+
+	VkSpecializationInfo *specialisationInfo = set_specialise_info(&extent);
+	VkVertexInputAttributeDescription *tofree = NULL;
+
+	VkPipelineShaderStageCreateInfo vertShaderStageInfo = {0};
+	vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+	vertShaderStageInfo.module = vert->shader;
+	vertShaderStageInfo.pName = "main";
+	vertShaderStageInfo.pSpecializationInfo = specialisationInfo;
+
+	VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+	fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	fragShaderStageInfo.module = frag->shader;
+	fragShaderStageInfo.pName = "main";
+
+	VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+	// So this should be cleverer, built ffrom the list objects
+	// And the vertexes for multiple objects, coallesced
+	VkVertexInputBindingDescription btmp;
+	if (binding_description == NULL) {
+		// FIXME: binding & attribute should both be set
+		// FIXME: Should pass these as a isngle struct to
+		// make the paramster sane
+		btmp = vertex_binding_description_get();
+		binding_description = &btmp;
+	}
+	bool free_attribute = false;
+	if (attribute_description == NULL) {
+		tofree = get_attribute_description_pair(&nattributes);
+		attribute_description = tofree;
+		free_attribute = true;
+	}
+
+	VkPipelineVertexInputStateCreateInfo vertexInputInfo = {0};
+	vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+	vertexInputInfo.vertexBindingDescriptionCount = 1;
+	vertexInputInfo.pVertexBindingDescriptions = binding_description;
+	vertexInputInfo.vertexAttributeDescriptionCount = nattributes;
+	vertexInputInfo.pVertexAttributeDescriptions = attribute_description;
+
+	VkPipelineInputAssemblyStateCreateInfo inputAssembly = {0};
+	inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+	inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+	inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+	VkViewport viewport = {0};
+	viewport.x = 0.0f;
+	viewport.y = 0.0f;
+	viewport.width = (float)extent.width;
+	viewport.height = (float)extent.height;
+	viewport.minDepth = 0.0f;
+	viewport.maxDepth = 1.0f;
+
+	VkRect2D scissor = {0};
+	scissor.offset.x = 0;
+	scissor.offset.y = 0;
+	scissor.extent = extent;
+
+	VkPipelineViewportStateCreateInfo viewportState = {0};
+	viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+	viewportState.viewportCount = 1;
+	viewportState.pViewports = &viewport;
+	viewportState.scissorCount = 1;
+	viewportState.pScissors = &scissor;
+
+	VkPipelineRasterizationStateCreateInfo rasterizer = {0};
+	rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+	rasterizer.depthClampEnable = VK_FALSE;
+
+	rasterizer.rasterizerDiscardEnable = VK_FALSE;
+
+	rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+	rasterizer.lineWidth = 1.0f;
+	rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+	rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+	rasterizer.depthBiasEnable = VK_FALSE;
+	rasterizer.depthBiasConstantFactor = 0.0f; // Optional
+	rasterizer.depthBiasClamp = 0.0f;	   // Optional
+	rasterizer.depthBiasSlopeFactor = 0.0f;	   // Optional
+
+	VkPipelineMultisampleStateCreateInfo multisampling = {0};
+	multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+	multisampling.sampleShadingEnable = VK_FALSE;
+	multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+	multisampling.minSampleShading = 1.0f;		// Optional
+	multisampling.pSampleMask = NULL;		// Optional
+	multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
+	multisampling.alphaToOneEnable = VK_FALSE;	// Optional
+
+	VkPipelineDepthStencilStateCreateInfo depth_stencil = {0};
+	depth_stencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+	depth_stencil.depthTestEnable = VK_TRUE;
+	depth_stencil.depthWriteEnable = VK_TRUE;
+	depth_stencil.depthCompareOp = VK_COMPARE_OP_LESS;
+	depth_stencil.depthBoundsTestEnable = VK_FALSE;
+	depth_stencil.stencilTestEnable = VK_FALSE;
+
+	VkPipelineColorBlendAttachmentState colorBlendAttachment = {0};
+	colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+					      VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+	// FIXME: Should be from ovjlayer
+	colorBlendAttachment.blendEnable = false; //layer_blend;
+	/* FIXME: Sort out if I can just use hte blnd flag.
+	 * this is the original; I think ignored if layer_blend is false 
+	colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;	 // Optional
+	colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+	colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;		 // Optional
+	colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;	 // Optional
+	colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+	colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;		 // Optional
+	*/
+	colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+	colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+	colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+	colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+	colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+	colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
+	VkPipelineColorBlendStateCreateInfo colorBlending = {0};
+	colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+	colorBlending.logicOpEnable = VK_FALSE;
+	colorBlending.logicOp = VK_LOGIC_OP_COPY; // Optional
+	colorBlending.attachmentCount = 1;
+	colorBlending.pAttachments = &colorBlendAttachment;
+	colorBlending.blendConstants[0] = 0.0f; // Optional
+	colorBlending.blendConstants[1] = 0.0f; // Optional
+	colorBlending.blendConstants[2] = 0.0f; // Optional
+	colorBlending.blendConstants[3] = 0.0f; // Optional
+
+	VkDynamicState dynamicStates[] = {VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_LINE_WIDTH};
+
+	VkPipelineDynamicStateCreateInfo dynamicState = {0};
+	dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+	dynamicState.dynamicStateCount = 2;
+	dynamicState.pDynamicStates = dynamicStates;
+
+	VkPipelineLayoutCreateInfo pipeline_layout_info = {0};
+	pipeline_layout_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+	pipeline_layout_info.setLayoutCount = 1;
+	pipeline_layout_info.pSetLayouts = &descriptor_set_layout;
+	pipeline_layout_info.pushConstantRangeCount = 0; // Optional
+	pipeline_layout_info.pPushConstantRanges = NULL; // Optional
+
+	if (vkCreatePipelineLayout(turtle->device, &pipeline_layout_info, NULL,
+				   &info->pipeline_layout) != VK_SUCCESS) {
+		error("failed to create pipeline layout!");
+	}
+
+	VkGraphicsPipelineCreateInfo pipelineInfo = {0};
+	pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+	pipelineInfo.stageCount = 2;
+	pipelineInfo.pStages = shaderStages;
+	pipelineInfo.pVertexInputState = &vertexInputInfo;
+	pipelineInfo.pInputAssemblyState = &inputAssembly;
+	pipelineInfo.pViewportState = &viewportState;
+	pipelineInfo.pRasterizationState = &rasterizer;
+	pipelineInfo.pMultisampleState = &multisampling;
+	// FIXME: First behidn a flag
+	pipelineInfo.pDepthStencilState = &depth_stencil;
+	pipelineInfo.pColorBlendState = &colorBlending;
+
+	pipelineInfo.layout = info->pipeline_layout;
+	pipelineInfo.renderPass = layer->render_pass;
+	pipelineInfo.subpass = 0;
+	pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+	VkResult rv = vkCreateGraphicsPipelines(turtle->device, VK_NULL_HANDLE, 1, &pipelineInfo,
+						NULL, &info->pipeline);
+	if (rv != VK_SUCCESS) {
+		error_msg(rv, "failed to create graphics pipeline!");
+	}
+
+	if (tofree) talloc_free(tofree);
+	talloc_free(specialisationInfo);
+
+	return info;
+}
