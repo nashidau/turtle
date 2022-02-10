2cfc6be8d8399eb74c2faa95905c71fb74d58e5e
Brett Nash <nash@nash.space>
2022-02-09 22:28:38 -0800

    seer: Partial conversion to use array notation for passed pointers.
    Hopefully a little cleaner code.

--- trtl_seer.h
+++ trtl_seer.h
@@ -21,29 +21,29 @@ struct trtl_seer_indexset {
 
 struct trtl_object;
 
-struct trtl_seer *trtl_seer_init(struct turtle *turtle, VkExtent2D extent,
-				 trtl_render_layer_t nlayers, const struct trtl_layer_info *info);
+struct trtl_seer *trtl_seer_init(struct turtle turtle[1], VkExtent2D extent,
+				 trtl_render_layer_t nlayers,
+				 const struct trtl_layer_info info[nlayers]);
 
-struct trtl_object *trtl_seer_predefined_object_add(const char *name, struct turtle *turtle,
-				    trtl_render_layer_t layer);
+struct trtl_object *trtl_seer_predefined_object_add(const char name[static 1], struct turtle turtle[1],
+						    trtl_render_layer_t layer);
 
-int trtl_seer_object_add(struct turtle *turtle, struct trtl_object *object,
+int trtl_seer_object_add(struct turtle turtle[1], struct trtl_object *object,
 			 trtl_render_layer_t layerid);
 
-int trtl_seer_resize(VkExtent2D new_size, struct turtle *turtle);
+int trtl_seer_resize(VkExtent2D new_size, struct turtle turtle[1]);
 
-int trtl_seer_draw(struct turtle *turtle, VkCommandBuffer buffer, trtl_render_layer_t layerid);
-int trtl_seer_update(struct turtle *turtle, uint32_t image_index);
+int trtl_seer_draw(struct turtle turtle[1], VkCommandBuffer buffer, trtl_render_layer_t layerid);
+int trtl_seer_update(struct turtle turtle[1], uint32_t image_index);
 struct trtl_seer_vertexset *trtl_seer_vertexes_get(trtl_render_layer_t, uint32_t *nobjects,
 						   uint32_t *nvertexes);
 struct trtl_seer_indexset *trtl_seer_indexset_get(trtl_render_layer_t, uint32_t *nobjects,
 						  uint32_t *nindexes);
 
 // FIXME: Hackery here
-trtl_must_check VkCommandBuffer *trtl_seer_create_command_buffers(struct turtle *turtle,
+trtl_must_check VkCommandBuffer *trtl_seer_create_command_buffers(struct turtle turtle[1],
 								  VkCommandPool command_pool);
 
 // FIXME: This shouldn;t be needed - it should be done automatically by object creation or
 // insertion or something
-struct trtl_strata *trtl_seer_strata_get(struct turtle *turtle, const char *name);
-
+struct trtl_strata *trtl_seer_strata_get(struct turtle turtle[1], const char name[static 1]);
