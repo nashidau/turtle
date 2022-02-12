a3f35b2de699ed3a66335e4c1a1d3c77b53a27a9
Brett Nash <nash@nash.space>
2022-02-11 17:52:11 -0800

    Add a new interface for a trtl_object_grid.

--- trtl_object.h
+++ trtl_object.h
@@ -17,3 +17,16 @@ struct trtl_object {
 	// The object should create it's pipeline based on the info in this call
 	void (*relayer)(struct trtl_object *obj, struct turtle *turtle, struct trtl_layer *layer);
 };
+
+// Grid object; trtl_grid_object
+// An object that works within a grid.
+// It has three additional properties;
+// 	x,y on the grid.
+// 	Facing on the grid
+struct trtl_grid_object {
+	struct trtl_object o;
+
+	bool (*move)(struct trtl_object *obj, uint32_t x, uint32_t y);
+
+	bool (*facing)(struct trtl_object *obj, int facing);
+};
