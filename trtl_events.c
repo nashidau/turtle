63213336632da6a97b26d69a6a831dd3d90b8589
Brett Nash <nash@nash.space>
2022-03-12 17:54:36 -0800

    events: Add a zoom event.

--- trtl_events.c
+++ trtl_events.c
@@ -2,22 +2,26 @@
 
 #include <talloc.h>
 
-#include "turtle.h"
 #include "trtl_events.h"
+#include "turtle.h"
 
 struct trtl_events *
-trtl_event_init(void) {
+trtl_event_init(void)
+{
 	struct trtl_events *events = talloc_zero(NULL, struct trtl_events);
 
 	events->crier = talloc_steal(events, trtl_crier_init());
-	
+
 	events->resize = trtl_crier_new_cry(events->crier, "trtl_event_resize");
 	events->grid_move = trtl_crier_new_cry(events->crier, "trtl_event_grid_move");
+	events->grid_zoom = trtl_crier_new_cry(events->crier, "trtl_event_grid_zoom");
 
 	return events;
 }
 
-int trtl_event_resize(struct turtle *turtle, VkExtent2D new_size) {
+int
+trtl_event_resize(struct turtle *turtle, VkExtent2D new_size)
+{
 	struct trtl_event_resize *resize = talloc_zero(NULL, struct trtl_event_resize);
 	struct trtl_events *events = turtle->events;
 	assert(resize);
@@ -26,12 +30,14 @@ int trtl_event_resize(struct turtle *turtle, VkExtent2D new_size) {
 	resize->new_size = new_size;
 
 	trtl_crier_post(events->crier, events->resize, resize);
-	
+
 	talloc_free(resize);
 	return 0;
 }
 
-int trtl_event_grid_move(struct turtle *turtle, int32_t x, int32_t y, bool snap) {
+int
+trtl_event_grid_move(struct turtle *turtle, int32_t x, int32_t y, bool snap)
+{
 	struct trtl_event_grid_move *gmove = talloc_zero(NULL, struct trtl_event_grid_move);
 	struct trtl_events *events = turtle->events;
 	assert(gmove);
@@ -42,8 +48,23 @@ int trtl_event_grid_move(struct turtle *turtle, int32_t x, int32_t y, bool snap)
 	gmove->snap = snap;
 
 	trtl_crier_post(events->crier, events->grid_move, gmove);
-	
+
 	talloc_free(gmove);
 	return 0;
 }
 
+int
+trtl_event_grid_zoom(struct turtle *turtle, uint32_t feature_size)
+{
+	struct trtl_event_grid_zoom *gzoom = talloc_zero(NULL, struct trtl_event_grid_zoom);
+	struct trtl_events *events = turtle->events;
+	assert(gzoom);
+
+	gzoom->turtle = turtle;
+	gzoom->feature_size = feature_size;
+
+	trtl_crier_post(events->crier, events->grid_zoom, gzoom);
+
+	talloc_free(gzoom);
+	return 0;
+}
--- trtl_events.h
+++ trtl_events.h
@@ -14,6 +14,7 @@ struct trtl_events {
 
 	trtl_crier_cry_t resize;
 	trtl_crier_cry_t grid_move;
+	trtl_crier_cry_t grid_zoom;
 };
 
 struct trtl_event_resize {
@@ -30,7 +31,14 @@ struct trtl_event_grid_move {
 	bool snap;
 };
 
+struct trtl_event_grid_zoom {
+	struct turtle *turtle;
+
+	uint32_t feature_size;
+};
+
 struct trtl_events *trtl_event_init(void);
 
 int trtl_event_resize(struct turtle *turtle, VkExtent2D new_size);
 int trtl_event_grid_move(struct turtle *turtle, int32_t x, int32_t y, bool snap);
+int trtl_event_grid_zoom(struct turtle *turtle, uint32_t zoom);
