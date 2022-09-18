89179d20322e1e0d1e7ef5a5eabaeef15518da84
Brett Nash <nash@nash.space>
2022-09-18 09:53:40 -0700

    pipeline: Set cullmode to non for now
    Add a fixme to make it a flag - it's pretty common choice.

--- trtl_pipeline.c
+++ trtl_pipeline.c
@@ -161,7 +161,10 @@ trtl_pipeline_create(struct turtle *turtle, VkRenderPass render_pass, VkExtent2D
 
 	rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
 	rasterizer.lineWidth = 1.0f;
-	rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+	// FIXME: Cull mode sould be customisable.
+	// A flag to set the appropriate way.
+	// NONE is probalby the worst choice here.
+	rasterizer.cullMode = VK_CULL_MODE_NONE;
 	rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 	rasterizer.depthBiasEnable = VK_FALSE;
 	rasterizer.depthBiasConstantFactor = 0.0f; // Optional
